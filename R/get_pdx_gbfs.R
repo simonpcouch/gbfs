ef56fac06774a035b79bb57faa57c0c218aec2d6
Simon Patrick Couch <simonpatrickcouch@gmail.com>
2018-04-03 22:25:54 -0700

    initial commit

---
+++ R/get_pdx_gbfs.R
@@ -0,0 +1,5 @@
+get_pdx_gbfs <- function() {
+get_free_bike_status()
+get_station_information()
+get_station_status()
+}
---
+++ R/get_pdx_status.R
@@ -0,0 +1,4 @@
+get_pdx_status <- function() {
+  get_free_bike_status()
+  get_station_status()
+}
