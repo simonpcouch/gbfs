d001fb14fa9258c24e90af96f75fcdbb5f523e94
Simon Patrick Couch <simonpatrickcouch@gmail.com>
2018-04-04 11:51:38 -0700

    add cumulative saving functionality

--- R/get_station_status.R
+++ R/get_station_status.R
@@ -13,9 +13,10 @@ station_status <- fromJSON(txt = station_status_feed)
 #extract data, convert to df
 station_status_data <- station_status$data$stations
 
-#class columns of station_status_data
+#class and rename columns of station_status_data
 station_status_data$last_reported <- as.POSIXct(station_status_data$last_reported, 
                                                origin = "1970-01-01")
+rename(station_status_data, last_updated = last_reported)
 station_status_data$num_bikes_disabled <- as.numeric(station_status_data$num_bikes_disabled)
 station_status_data$is_installed <- as.logical(station_status_data$is_installed)
 station_status_data$is_renting <- as.logical(station_status_data$is_renting)
@@ -25,15 +26,17 @@ station_status_data$is_returning <- as.logical(station_status_data$is_returning)
 station_status_last_updated <- station_status$last_updated %>%
   as.POSIXct(., origin = "1970-01-01")
 
-#mutate new last_updated column
-station_status_data <- station_status_data %>%
-  mutate(last_updated = station_status_last_updated)
-
 #extract time til next update (in seconds), convert to numeric
 station_status_ttl <- station_status$ttl %>%
   as.numeric()
 
-#save() results in a smaller file size and allows for easier `rbind`ing than write_csv()
-#write_csv(x = station_status_data, path = "data-raw/station_status.csv")
-save(station_status_data, file = "~/bikeshare-1/data-raw/station_status_data.rda")
+update_ss <- function() {
+  ss <- readRDS("~/bikeshare-1/data/ss.rds")
+  ss_update <- rbind(station_status_data, ss)
+  saveRDS(ss_update, file = "~/bikeshare-1/data/ss.rds")
+}
+
+ifelse(file.exists("~/bikeshare-1/data/ss.rds"), 
+       update_ss(),
+       saveRDS(station_status_data, file = "~/bikeshare-1/data/ss.rds"))
 }
