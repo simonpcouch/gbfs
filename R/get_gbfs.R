878a7132fa71ea1dfa46fa70064b0b1940793950
Simon Patrick Couch <simonpatrickcouch@gmail.com>
2018-04-24 18:02:16 -0700

    assign to function (not global) environment

--- R/get_gbfs.R
+++ R/get_gbfs.R
@@ -24,58 +24,58 @@ get_gbfs_feeds <- function(url) {
 
   feeds <- gbfs$data$en$feeds
 
-  stat_info_feed <<- feeds %>%
+  stat_info_feed <- feeds %>%
     select(url) %>%
     filter(str_detect(url, "station_information")) %>%
     as.character()
 
-  sys_info_feed <<- feeds %>%
+  sys_info_feed <- feeds %>%
     select(url) %>%
     filter(str_detect(url, "system_information")) %>%
     as.character()
 
-  stat_status_feed <<- feeds %>%
+  stat_status_feed <- feeds %>%
     select(url) %>%
     filter(str_detect(url, "station_status")) %>%
     as.character()
 
   if ("free_bike_status" %in% feeds$name) {
-    fbs_feed <<- feeds %>%
+    fbs_feed <- feeds %>%
       select(url) %>%
       filter(str_detect(url, "free_bike_status")) %>%
       as.character()
   }
 
   if ("system_hours" %in% feeds$name) {
-    sys_hours_feed <<- feeds %>%
+    sys_hours_feed <- feeds %>%
       select(url) %>%
       filter(str_detect(url, "system_hours")) %>%
       as.character()
   }
 
   if ("system_calendar" %in% feeds$name) {
-    sys_cal_feed <<- feeds %>%
+    sys_cal_feed <- feeds %>%
       select(url) %>%
       filter(str_detect(url, "system_calendar")) %>%
       as.character()
   }
 
   if ("system_regions" %in% feeds$name) {
-    sys_reg_feed <<- feeds %>%
+    sys_reg_feed <- feeds %>%
       select(url) %>%
       filter(str_detect(url, "system_regions")) %>%
       as.character()
   }
 
   if ("system_pricing_plans" %in% feeds$name) {
-    sys_price_feed <<- feeds %>%
+    sys_price_feed <- feeds %>%
       select(url) %>%
       filter(str_detect(url, "system_pricing_plans")) %>%
       as.character()
   }
 
   if ("system_alerts" %in% feeds$name) {
-    sys_alerts_feed <<- feeds %>%
+    sys_alerts_feed <- feeds %>%
       select(url) %>%
       filter(str_detect(url, "system_alerts")) %>%
       as.character()
