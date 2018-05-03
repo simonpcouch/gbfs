6792a846727c839d02d26fa86c6d5ff5dac94fdd
Simon Patrick Couch <simonpatrickcouch@gmail.com>
2018-05-03 08:39:54 -0700

    comment out code; save for future dev

--- R/get_station_status_automation_attempt.R
+++ R/get_station_status_automation_attempt.R
@@ -1,46 +1,46 @@
-get_station_status <- function(url, filepath = "data/station_status.rds", cumulative = FALSE) {
+#get_station_status <- function(url, filepath = "data/station_status.rds", cumulative = FALSE) {
 
-#test station_status_feed: "http://biketownpdx.socialbicycles.com/opendata/station_status.json"  
+  #test station_status_feed: "http://biketownpdx.socialbicycles.com/opendata/station_status.json"  
   
-#get url
-station_status_feed <- url
+  #get url
+#station_status_feed <- url
 
-#save feed
-station_status <- fromJSON(txt = station_status_feed)
+  #save feed
+#station_status <- fromJSON(txt = station_status_feed)
 
-#extract data, convert to df
-station_status_data <- station_status$data$stations
+  #extract data, convert to df
+#station_status_data <- station_status$data$stations
 
-#extract time til next update (in seconds), convert to numeric
-station_status_ttl <- station_status$ttl %>%
-  as.numeric()
+  #extract time til next update (in seconds), convert to numeric
+#station_status_ttl <- station_status$ttl %>%
+#  as.numeric()
 
-#save the new data, method depending on cumulative argument
-if (cumulative == TRUE) {
-    if (!file.exists(filepath)) {
+  #save the new data, method depending on cumulative argument
+#if (cumulative == TRUE) {
+#    if (!file.exists(filepath)) {
       #save the initial file
-      saveRDS(station_status_data, file = filepath)
+#      saveRDS(station_status_data, file = filepath)
       #add a cron job or task schedule, depending on os
-        if (.Platform$OS.type == "windows") {
+#        if (.Platform$OS.type == "windows") {
         #code for taskscheduleR
-        return("Automation functionality has not yet been implemented for Windows machines.")
-      } else {
-        arg1 <- paste0("station_status_feed = ", station_status_feed)
-        arg2 <- paste0("filepath = ", filepath)
+#        return("Automation functionality has not yet been implemented for Windows machines.")
+#      } else {
+#        arg1 <- paste0("station_status_feed = ", station_status_feed)
+#        arg2 <- paste0("filepath = ", filepath)
         #code for cronR
-        cmd <- cron_rscript(rscript = 'R/update_ss.R', 
-                            rscript_args = c(arg1, arg2))
-        cron_add(command = cmd, 
-                 frequency = 'minutely', 
-                 id = 'get_station_status', 
-                 description = 'Grabs every minute.')   
+#        cmd <- cron_rscript(rscript = 'R/update_ss.R', 
+#                            rscript_args = c(arg1, arg2))
+#        cron_add(command = cmd, 
+#                 frequency = 'minutely', 
+#                 id = 'get_station_status', 
+#                 description = 'Grabs every minute.')   
       
-    }} else {
-    #append rows
-    ss <- readRDS(filepath)
-    ss_update <- rbind(station_status_data, ss)
-    saveRDS(ss_update, file = filepath)
-  }} else {
+#    }} else {
+#    #append rows
+#    ss <- readRDS(filepath)
+#    ss_update <- rbind(station_status_data, ss)
+#    saveRDS(ss_update, file = filepath)
+#  }} else {
     #code for when cumulative is false: just save the file
-    saveRDS(station_status_data, file = filepath)
-  }}
+#    saveRDS(station_status_data, file = filepath)
+#  }}
