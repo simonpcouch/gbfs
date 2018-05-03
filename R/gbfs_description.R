0900b49fe6bd15e1c8ac50eb94259c98ab506b91
Simon Patrick Couch <simonpatrickcouch@gmail.com>
2018-05-03 10:06:16 -0700

    initial commit

---
+++ R/gbfs_description.R
@@ -0,0 +1,16 @@
+#' Package: gbfs
+#'
+#' General Bikeshare Feed Specification
+#'
+#' Description
+#'
+#' @section gbfs Functions:
+#' get_gbfs(), get_free_bike_status(), get_station_information(),
+#' get_station_status(), get_system_alerts(), get_system_calendar(),
+#' get_system_hours(), get_system_information(),
+#' get_system_pricing_plans(), get_system_regions()
+#'
+#' @docType package
+#' @name gbfs
+#' @importFrom dplyr %>%
+NULL
