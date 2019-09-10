5731fb7dd9d2f77300dd0fe84f5d177fdeccdcfc
Simon Couch <simonpatrickcouch@gmail.com>
2019-09-09 21:21:54 -0700

    add new `output` arg to static feeds!

--- R/get_static_feeds.R
+++ R/get_static_feeds.R
@@ -1,23 +1,36 @@
-#' Save the station_information feed.
+#' Grab the station_information feed.
 #' 
-#' \code{get_station_information} saves the station_information feed for a given city as 
-#' a .rds object. Go to `https://github.com/NABSA/gbfs/blob/master/gbfs.md` to see 
-#' metadata for this dataset.
+#' \code{get_station_information} grabs and tidies the station_information feed for a given city. 
+#' Go to `https://github.com/NABSA/gbfs/blob/master/gbfs.md` to see metadata for this dataset.
 #' 
 #' @param city A character string or a url to an active gbfs.json feed. See \code{get_gbfs_cities}
 #' for a current list of available cities.
 #' @param directory The name of an existing folder or folder to be created, where the feed will
 #'   will be saved.
-#' @param file The name of the file to be saved. Must end in .rds.
-#' @return A .rds object generated from the current station_information feed.
+#' @param file The name of the file to be saved (if output is default). Must end in .rds.
+#' @param output The type of output method. If `output = "save"`, the object will be saved as
+#' an .rds object at the given path. If `output = "return"`, the output will be returned
+#' as a dataframe object. Setting `output = "both"` will do both.
+#' @return Depends on argument to `output`: Either a saved .rds object generated from the 
+#' current station_information feed, a dataframe object, or both
 #' @examples
 #' \donttest{get_station_information(city = 
 #' "http://biketownpdx.socialbicycles.com/opendata/station_information.json",  directory = tempdir())}
 #' \donttest{get_station_information(city = "Battle Creek",  directory = tempdir())}
 #' @export
 
-get_station_information <- function(city, directory, file = "station_information.rds") {
+get_station_information <- function(city, directory = NULL, file = "station_information.rds", output = "save") {
 
+  if (!output %in% c("save", "return", "both")) {
+    stop(sprintf("Please supply one of \"save\", \"return\", or \"both\" as arguments to `output`."))
+  }
+  
+  if (is.null(directory) & output %in% c("save", "both")) {
+    stop(sprintf("You have not supplied a location to save the resulting file, but the supplied arguments
+    indicate that you'd like to save the dataframe. Please supply a `directory` argument or 
+    set `output = \"return\".`"))
+  }
+  
   url <- city_to_url(city)
   
   if (url != city) {
@@ -49,6 +62,7 @@ get_station_information <- function(city, directory, file = "station_information
   station_information_last_updated <- station_information$last_updated %>%
     as.POSIXct(., origin = "1970-01-01")
   
+  if (output %in% c("save", "both")) {
   # create directory
   if (!dir.exists(directory)) {
     dir.create(directory)
@@ -57,11 +71,16 @@ get_station_information <- function(city, directory, file = "station_information
   #save() results in a smaller file size and allows for easier `rbind`ing than write_csv()
   #write_csv(x = station_information_data, path = "data-raw/station_information.csv")
   saveRDS(station_information_data, file = paste(directory, file, sep = "/"))
+  } 
+  
+  if (output %in% c("return", "both")) {
+    station_information_data
+  }
 }
 
-#' Save the system_alerts feed.
+#' Grab the system_alerts feed.
 #' 
-#' \code{get_system_alerts} saves the system_alerts feed for a given city as a .rds object. Go to 
+#' \code{get_system_alerts} grabs and tidies the system_alerts feed for a given city. Go to 
 #' `https://github.com/NABSA/gbfs/blob/master/gbfs.md` to see metadata for this dataset.
 #' 
 #' @param city A character string or a url to an active gbfs.json feed. See \code{get_gbfs_cities}
@@ -69,15 +88,29 @@ get_station_information <- function(city, directory, file = "station_information
 #' @param directory The name of an existing folder or folder to be created, where the feed will
 #'   will be saved.
 #' @param file The name of the file to be saved. Must end in .rds.
-#' @return A .rds object generated from the current system_alerts feed.
+#' @param output The type of output method. If `output = "save"`, the object will be saved as
+#' an .rds object at the given path. If `output = "return"`, the output will be returned
+#' as a dataframe object. Setting `output = "both"` will do both.
+#' @return Depends on argument to `output`: Either a saved .rds object generated from the 
+#' current system_alerts feed, a dataframe object, or both.
 #' @examples
 #' \donttest{get_system_alerts(city = 
 #' "http://biketownpdx.socialbicycles.com/opendata/system_alerts.json", directory = tempdir())}
 #' \donttest{get_system_alerts(city = "Santa Monica", directory = tempdir())}
 #' @export
 
-get_system_alerts <- function (city, directory, file = "system_alerts.rds") {
+get_system_alerts <- function (city, directory = NULL, file = "system_alerts.rds", output = "save") {
 
+  if (!output %in% c("save", "return", "both")) {
+    stop(sprintf("Please supply one of \"save\", \"return\", or \"both\" as arguments to `output`."))
+  }
+  
+  if (is.null(directory) & output %in% c("save", "both")) {
+    stop(sprintf("You have not supplied a location to save the resulting file, but the supplied arguments
+    indicate that you'd like to save the dataframe. Please supply a `directory` argument or 
+    set `output = \"return\".`"))
+  }  
+  
   url <- city_to_url(city)
   
   if (url != city) {
@@ -104,18 +137,23 @@ get_system_alerts <- function (city, directory, file = "system_alerts.rds") {
   system_alerts_last_updated <- system_alerts$last_updated %>%
     as.POSIXct(., origin = "1970-01-01")
   
+  
+  if (output %in% c("save", "both")) {
   # create directory
   if (!dir.exists(directory)) {
     dir.create(directory)
   }
 
   saveRDS(system_alerts_data, file = paste(directory, file, sep = "/"))
-
+  }
+  if (output %in% c("return", "both")) {
+    system_alerts_data
+  }
 }
 
-#' Save the system_calendar feed.
+#' Grab the system_calendar feed.
 #' 
-#' \code{get_system_calendar} saves the system_calendar feed for a given city as a .rds object. Go to 
+#' \code{get_system_calendar} grabs and tidies the system_calendar feed for a given city. Go to 
 #' `https://github.com/NABSA/gbfs/blob/master/gbfs.md` to see metadata for this dataset.
 #' 
 #' @param city A character string or a url to an active gbfs.json feed. See \code{get_gbfs_cities}
@@ -123,15 +161,29 @@ get_system_alerts <- function (city, directory, file = "system_alerts.rds") {
 #' @param directory The name of an existing folder or folder to be created, where the feed will
 #'   will be saved.
 #' @param file The name of the file to be saved. Must end in .rds.
-#' @return A .rds object generated from the current system_calendar feed.
+#' @param output The type of output method. If `output = "save"`, the object will be saved as
+#' an .rds object at the given path. If `output = "return"`, the output will be returned
+#' as a dataframe object. Setting `output = "both"` will do both.
+#' @return Depends on argument to `output`: Either a saved .rds object generated from the 
+#' current system_alerts feed, a dataframe object, or both
 #' @examples
 #' \donttest{get_system_calendar(city = 
 #' "http://biketownpdx.socialbicycles.com/opendata/system_calendar.json", directory = tempdir())}
 #' \donttest{get_system_calendar(city = "Tampa", directory = tempdir())}
 #' @export
 
-get_system_calendar <- function (city, directory, file = "system_calendar.rds") {
+get_system_calendar <- function (city, directory = NULL, file = "system_calendar.rds", output = "save") {
 
+  if (!output %in% c("save", "return", "both")) {
+    stop(sprintf("Please supply one of \"save\", \"return\", or \"both\" as arguments to `output`."))
+  }
+  
+  if (is.null(directory) & output %in% c("save", "both")) {
+    stop(sprintf("You have not supplied a location to save the resulting file, but the supplied arguments
+    indicate that you'd like to save the dataframe. Please supply a `directory` argument or 
+    set `output = \"return\".`"))
+  }  
+  
   url <- city_to_url(city)
   
   if (url != city) {
@@ -162,18 +214,23 @@ get_system_calendar <- function (city, directory, file = "system_calendar.rds") 
   system_calendar_ttl <- system_calendar$ttl %>%
     as.numeric()
   
+  if (output %in% c("save", "both")) {
   # create directory
   if (!dir.exists(directory)) {
     dir.create(directory)
   }
 
   saveRDS(system_calendar_data, file = paste(directory, file, sep = "/"))
-
+  }
+  
+  if (output %in% c("return", "both")) {
+    system_calendar_data
+  }
 }
 
-#' Save the system_hours feed.
+#' Grab the system_hours feed.
 #' 
-#' \code{get_system_hours} saves the system_hours feed for a given city as a .rds object. Go to 
+#' \code{get_system_hours} grabs and tidies the system_hours feed for a given city. Go to 
 #' `https://github.com/NABSA/gbfs/blob/master/gbfs.md` to see metadata for this dataset.
 #' 
 #' @param city A character string or a url to an active gbfs.json feed. See \code{get_gbfs_cities}
@@ -181,15 +238,29 @@ get_system_calendar <- function (city, directory, file = "system_calendar.rds") 
 #' @param directory The name of an existing folder or folder to be created, where the feed will
 #'   will be saved.
 #' @param file The name of the file to be saved. Must end in .rds.
-#' @return A .rds object generated from the current system_hours feed.
+#' @param output The type of output method. If `output = "save"`, the object will be saved as
+#' an .rds object at the given path. If `output = "return"`, the output will be returned
+#' as a dataframe object. Setting `output = "both"` will do both.
+#' @return Depends on argument to `output`: Either a saved .rds object generated from the 
+#' current system_alerts feed, a dataframe object, or both.
 #' @examples
 #' \donttest{get_system_hours(city = 
 #' "http://biketownpdx.socialbicycles.com/opendata/system_hours.json", directory = tempdir())}
 #' \donttest{get_system_hours(city = "Phoenix", directory = tempdir())}
 #' @export
 
-get_system_hours <- function (city, directory, file = "system_hours.rds") {
+get_system_hours <- function (city, directory = NULL, file = "system_hours.rds", output = "save") {
 
+  if (!output %in% c("save", "return", "both")) {
+    stop(sprintf("Please supply one of \"save\", \"return\", or \"both\" as arguments to `output`."))
+  }
+  
+  if (is.null(directory) & output %in% c("save", "both")) {
+    stop(sprintf("You have not supplied a location to save the resulting file, but the supplied arguments
+    indicate that you'd like to save the dataframe. Please supply a `directory` argument or 
+    set `output = \"return\".`"))
+  }
+  
   url <- city_to_url(city)
   
   if (url != city) {
@@ -222,18 +293,25 @@ get_system_hours <- function (city, directory, file = "system_hours.rds") {
   system_hours_last_updated <- system_hours$last_updated %>%
     as.POSIXct(., origin = "1970-01-01")
 
+  
+  if (output %in% c("save", "both")) {
   # create directory
   if (!dir.exists(directory)) {
     dir.create(directory)
   }
   
   saveRDS(system_hours_data, file = paste(directory, file, sep = "/"))
-
+  }
+  
+  if (output %in% c("return", "both")) {
+   system_hours_data 
+  }
+  
 }
 
-#' Save the system_information feed.
+#' Grab the system_information feed.
 #' 
-#' \code{get_system_information} saves the system_information feed for a given city as a .rds object. Go to 
+#' \code{get_system_information} grabs and tidies the system_information feed for a given city. Go to 
 #' `https://github.com/NABSA/gbfs/blob/master/gbfs.md` to see metadata for this dataset.
 #' 
 #' @param city A character string or a url to an active gbfs.json feed. See \code{get_gbfs_cities}
@@ -241,15 +319,29 @@ get_system_hours <- function (city, directory, file = "system_hours.rds") {
 #' @param directory The name of an existing folder or folder to be created, where the feed will
 #'   will be saved.
 #' @param file The name of the file to be saved. Must end in .rds.
-#' @return A .rds object generated from the current system_information feed.
+#' @param output The type of output method. If `output = "save"`, the object will be saved as
+#' an .rds object at the given path. If `output = "return"`, the output will be returned
+#' as a dataframe object. Setting `output = "both"` will do both.
+#' @return Depends on argument to `output`: Either a saved .rds object generated from the 
+#' current system_alerts feed, a dataframe object, or both.
 #' @examples
 #' \donttest{get_system_information(city = 
 #' "http://biketownpdx.socialbicycles.com/opendata/system_information.json", directory = tempdir())}
 #' \donttest{get_system_information(city = "Omaha",  directory = tempdir())}
 #' @export
 
-get_system_information <- function(city, directory, file = "system_information.rds"){
+get_system_information <- function(city, directory = NULL, file = "system_information.rds", output = "save"){
 
+  if (!output %in% c("save", "return", "both")) {
+    stop(sprintf("Please supply one of \"save\", \"return\", or \"both\" as arguments to `output`."))
+  }
+  
+  if (is.null(directory) & output %in% c("save", "both")) {
+    stop(sprintf("You have not supplied a location to save the resulting file, but the supplied arguments
+    indicate that you'd like to save the dataframe. Please supply a `directory` argument or 
+    set `output = \"return\".`"))
+  }
+  
   url <- city_to_url(city)
   
   if (url != city) {
@@ -278,18 +370,24 @@ get_system_information <- function(city, directory, file = "system_information.r
   system_information_last_updated <- system_information$last_updated %>%
     as.POSIXct(., origin = "1970-01-01")
 
+  if (output %in% c("save", "both")) {
   # create directory
   if (!dir.exists(directory)) {
     dir.create(directory)
   }
 
   saveRDS(system_information_data, file = paste(directory, file, sep = "/"))
-
+  }
+  
+  if (output %in% c("return", "both")) {
+    system_information_data
+  }
+    
 }
 
-#' Save the system_pricing_plans feed.
+#' Grab the system_pricing_plans feed.
 #' 
-#' \code{get_system_pricing_plans} saves the system_pricing_plans feed for a given city as a .rds object. Go to 
+#' \code{get_system_pricing_plans} grabs and tidies the system_pricing_plans feed for a given city. Go to 
 #' `https://github.com/NABSA/gbfs/blob/master/gbfs.md` to see metadata for this dataset.
 #' 
 #' @param city A character string or a url to an active gbfs.json feed. See \code{get_gbfs_cities}
@@ -297,15 +395,29 @@ get_system_information <- function(city, directory, file = "system_information.r
 #' @param directory The name of an existing folder or folder to be created, where the feed will
 #'   will be saved.
 #' @param file The name of the file to be saved. Must end in .rds.
-#' @return A .rds object generated from the current system_pricing_plans feed.
+#' @param output The type of output method. If `output = "save"`, the object will be saved as
+#' an .rds object at the given path. If `output = "return"`, the output will be returned
+#' as a dataframe object. Setting `output = "both"` will do both.
+#' @return Depends on argument to `output`: Either a saved .rds object generated from the 
+#' current system_alerts feed, a dataframe object, or both.
 #' @examples
 #' \donttest{get_system_pricing_plans(city = 
 #' "http://biketownpdx.socialbicycles.com/opendata/system_pricing_plans.json", directory = tempdir())}
 #' \donttest{get_system_pricing_plans(city = "Houston", directory = tempdir())}
 #' @export
 
-get_system_pricing_plans <- function(city, directory, file = "system_pricing_plans.rds") {
+get_system_pricing_plans <- function(city, directory = NULL, file = "system_pricing_plans.rds", output = "save") {
 
+  if (!output %in% c("save", "return", "both")) {
+    stop(sprintf("Please supply one of \"save\", \"return\", or \"both\" as arguments to `output`."))
+  }
+  
+  if (is.null(directory) & output %in% c("save", "both")) {
+    stop(sprintf("You have not supplied a location to save the resulting file, but the supplied arguments
+    indicate that you'd like to save the dataframe. Please supply a `directory` argument or 
+    set `output = \"return\".`"))
+  }  
+  
   url <- city_to_url(city)
   
   if (url != city) {
@@ -335,18 +447,23 @@ get_system_pricing_plans <- function(city, directory, file = "system_pricing_pla
   system_pricing_plans_last_updated <- system_pricing_plans$last_updated %>%
     as.POSIXct(., origin = "1970-01-01")
 
+  if (output %in% c("save", "both")) {
   # create directory
   if (!dir.exists(directory)) {
     dir.create(directory)
   }
 
   saveRDS(system_pricing_plans_data, file = paste(directory, file, sep = "/"))
-
+  }
+  
+  if (output %in% c("return", "both")) {
+    system_pricing_plans_data 
+  }
 }
 
-#' Save the system_regions feed.
+#' Grab the system_regions feed.
 #' 
-#' \code{get_system_regions} saves the system_regions feed for a given city as a .rds object. Go to 
+#' \code{get_system_regions} grabs and tidies the system_regions feed for a given city. Go to 
 #' `https://github.com/NABSA/gbfs/blob/master/gbfs.md` to see metadata for this dataset.
 #' 
 #' @param city A character string or a url to an active gbfs.json feed. See \code{get_gbfs_cities}
@@ -354,15 +471,29 @@ get_system_pricing_plans <- function(city, directory, file = "system_pricing_pla
 #' @param directory The name of an existing folder or folder to be created, where the feed will
 #'   will be saved.
 #' @param file The name of the file to be saved. Must end in .rds.
-#' @return A .rds object generated from the current system_regions feed.
+#' @param output The type of output method. If `output = "save"`, the object will be saved as
+#' an .rds object at the given path. If `output = "return"`, the output will be returned
+#' as a dataframe object. Setting `output = "both"` will do both.
+#' @return Depends on argument to `output`: Either a saved .rds object generated from the 
+#' current system_alerts feed, a dataframe object, or both.
 #' @examples
 #' \donttest{get_system_regions(city = 
 #' "http://biketownpdx.socialbicycles.com/opendata/system_regions.json", directory = tempdir())}
 #' \donttest{get_system_regions(city = "Boston", directory = tempdir())}
 #' @export
 
-get_system_regions <- function (city, directory, file = "system_regions.rds") {
+get_system_regions <- function (city, directory = NULL, file = "system_regions.rds", output = "save") {
 
+  if (!output %in% c("save", "return", "both")) {
+    stop(sprintf("Please supply one of \"save\", \"return\", or \"both\" as arguments to `output`."))
+  }
+  
+  if (is.null(directory) & output %in% c("save", "both")) {
+    stop(sprintf("You have not supplied a location to save the resulting file, but the supplied arguments
+    indicate that you'd like to save the dataframe. Please supply a `directory` argument or 
+    set `output = \"return\".`"))
+  }  
+  
   url <- city_to_url(city)
   
   if (url != city) {
@@ -389,11 +520,16 @@ get_system_regions <- function (city, directory, file = "system_regions.rds") {
   system_regions_last_updated <- system_regions$last_updated %>%
     as.POSIXct(., origin = "1970-01-01")
 
+  if (output %in% c("save", "both")) {
   # create directory
   if (!dir.exists(directory)) {
     dir.create(directory)
   }
 
   saveRDS(system_regions_data, file = paste(directory, file, sep = "/"))
-
+  }
+  
+  if (output %in% c("return", "both")) {
+    system_regions_data 
+  }
 }
