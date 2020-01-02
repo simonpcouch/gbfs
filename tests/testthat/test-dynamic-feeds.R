87f0d014c3d2694588c5229d7058c985775e7f86
Simon Couch <simonpatrickcouch@gmail.com>
2020-01-02 14:54:02 -0600

    revert to tempdir, add `check = TRUE`

--- tests/testthat/test-dynamic-feeds.R
+++ tests/testthat/test-dynamic-feeds.R
@@ -11,23 +11,21 @@ test_that("dynamic feeds work", {
 
 test_that("file saving and overwriting works", {
   
-  # pull up the working directory
-  dir <- getwd()
+  # make a temporary directory
+  dir <- tempdir(check = TRUE)
   
   # save the file to a subdirectory of it
-  get_station_status("portland", paste0(dir, "/testthatTests"))
+  get_station_status("portland", paste0(dir, "/test"))
   
   # ...and then append to it
-  get_station_status("portland", paste0(dir, "/testthatTests"))
-  
-  unlink(paste0(dir, "/testthatTests"), recursive = TRUE)
+  get_station_status("portland", paste0(dir, "/test"))
   
 })
 
 test_that("row binding checks work", {
-
-  # make a folder in the working directory
-  dir <- paste0(getwd(), "/testthatTests")
+  
+  # make a temporary directory
+  dir <- tempdir(check = TRUE)
   
   # grab some data to work with
   pdx_status <- get_station_status("portland", dir, output = "both")
@@ -39,28 +37,26 @@ test_that("row binding checks work", {
     dplyr::select(-station_id)
   
   expect_error(datasets_can_be_row_binded(
-                 pdx_status_, 
-                 paste0(dir, "/station_status.Rds")),
-               "columns, while")
+    pdx_status_, 
+    paste0(dir, "/station_status.Rds")),
+    "columns, while")
   
   # ... or a column name changed
   pdx_status_ <- pdx_status %>%
     dplyr::rename(stationId = station_id)
   
   expect_error(datasets_can_be_row_binded(
-                 pdx_status_, 
-                 paste0(dir, "/station_status.Rds")),
-               "has different column names")
+    pdx_status_, 
+    paste0(dir, "/station_status.Rds")),
+    "has different column names")
   
   # ... or different column types
   pdx_status_ <- pdx_status %>%
     dplyr::mutate(station_id = as.factor(station_id))
   
   expect_error(datasets_can_be_row_binded(
-                 pdx_status_, 
-                 paste0(dir, "/station_status.Rds")),
-               "has different column types")  
+    pdx_status_, 
+    paste0(dir, "/station_status.Rds")),
+    "has different column types")  
   
-  # get rid of the test files
-  unlink(paste0(dir, "/testthatTests"), recursive = TRUE)
 })
